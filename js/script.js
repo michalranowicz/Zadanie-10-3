6d386e762cb8aa9f471421dcd3fb4de33e420a16
Michał Ranowicz <michal.ranowicz@gmail.com>
2018-06-10 15:44:30 +0200

    added changes

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 $(function() {
   //this code will execute after the DOM is loaded
   setInterval(changeSlide, 3000);
-  var carouselList = $("#carousel ul");
+  var $carouselList = $("#carousel ul");
 
   function changeSlide() {
-    carouselList.animate({ marginLeft: -400 }, 500, moveFirstSlide);
+    $carouselList.fadeIn(5000, moveFirstSlide());
   }
   function moveFirstSlide() {
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");
+    var firstItem = $carouselList.find("li:first");
+    var lastItem = $carouselList.find("li:last");
 
     lastItem.after(firstItem);
-    carouselList.css({ marginLeft: 0 });
   }
   function moveLastSlide() {
-    var firstItem = carouselList.find("li:last");
-    var lastItem = carouselList.find("li:first");
+    var firstItem = $carouselList.find("li:last");
+    var lastItem = $carouselList.find("li:first");
 
     lastItem.before(firstItem);
-    carouselList.css({ marginLeft: 0 });
   }
-  $("#right-button").on("click", moveFirstSlide);
-  $("#left-button").on("click", moveLastSlide);
+  $("#right-button").click(function() {
+    moveFirstSlide();
+  });
+  $("#left-button").click(function() {
+    moveLastSlide();
+  });
 });
